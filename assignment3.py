98ae35aaf514665c482de59b2c546b4f96fe8712
Joy <146790408+joy-totheworld@users.noreply.github.com>
2024-02-24 20:55:02 -0800

    Update assignment3.py

--- assignment3.py
+++ assignment3.py
@@ -40,11 +40,11 @@ def minimum_cost_connecting_edges(input_file_path, output_file_path):
         # taking a string containing E prime edge data and storing it in    
         # a matrix representation of array E Prime, with one collum for each
         # of the two index numbers of each coordinate
-        QStringArray = re.findall('(\\-?\\d+,\\-?\\d+)', ArrayEPrimetxt)
+        EPrimeStringArray = re.findall('(\\-?\\d+,\\-?\\d+)', ArrayEPrimetxt)
         EPrimeNumElements = ( ArrayEPrimetxt.count(",") // 2 ) +1
         ArrayEPrime = [[-1 for column in range(2)] for row in range(EPrimeNumElements)]
         for e in range(0, EPrimeNumElements):
-            CoordinateE = QStringArray[e].split(',')
+            CoordinateE = EPrimeStringArray[e].split(',')
             ArrayEPrime[e][0]= int(CoordinateE[0])
             ArrayEPrime[e][1]= int(CoordinateE[1])
     
@@ -75,47 +75,94 @@ def minimum_cost_connecting_edges(input_file_path, output_file_path):
 
     # Creating a container to store connected components of our graph.
     Forrest = []
+    NumTreesInForrest = 0
 
     # Filling out forrest in edge case where E Prime is empty
     if (EPrimeNumElements == 0):
         for i in range(0, VNumVertices):
             Tree = [ArrayPoints[i]]
-            Forrest.insert(Tree)
-
+            Forrest.insert(i, Tree)
+            NumTreesInForrest = NumTreesInForrest + 1
+    
     # Filling out forrest in standard case where E Prime is not empty by inserting every connected component from E Prime
     elif (EPrimeNumElements > 0):
-        # base case. adding the two vertices from the first edge of E Prime to a new tree
-        IndexA = ArrayEPrime[0][0] 
-        IndexB = ArrayEPrime[0][1]
-        ArrayPoints[IndexA][2] = 1
-        ArrayPoints[IndexB][2] = 1
-        Tree = [ArrayPoints[IndexA]], [ArrayPoints[IndexB]]
-        Forrest.insert(Tree)
+        print("ArrayPoints", ArrayPoints)
 
         # visit each edge in E Prime and update its points' tree values accordingly
-        TreeIdentifier = 1
-        for i in range(1, EPrimeNumElements):
-            IndexA = ArrayEPrime[i][0] 
-            IndexB = ArrayEPrime[i][1]
-            #if neither of the edge's points have been added to a tree, add them to a new tree
-            if( (ArrayPoints[IndexA][2] == -1) and (ArrayPoints[IndexB][2] == -1) )
+        TreeIdentifier = 0
+        for i in range(0, EPrimeNumElements):
+
+
+            CoordinateNumA = ArrayEPrime[i][0] 
+            CoordinateNumB = ArrayEPrime[i][1]
+            print("CoordinateNumA: ", CoordinateNumA)
+            print("CoordinateNumB: ", CoordinateNumB)
+
+            IndexA = ArrayEPrime[i][0] - 1
+            IndexB = ArrayEPrime[i][1] - 1
+            print("IndexA: ", IndexA)
+            print("IndexB: ", IndexB)
+
+            print("ArrayPoints[IndexA]: ", ArrayPoints[IndexA])
+            print("ArrayPoints[IndexB]: ", ArrayPoints[IndexB])
+
+            #if neither of the edge's points have been added to a tree, add them to a new tree and remove them from the array of all other points
+            if( (ArrayPoints[IndexA][2] == -1) and (ArrayPoints[IndexB][2] == -1) ):
                 ArrayPoints[IndexA][2] = TreeIdentifier
                 ArrayPoints[IndexB][2] = TreeIdentifier
+                Tree = [[ArrayPoints[IndexA]], [ArrayPoints[IndexB]]]
+                print("ArrayPoints", ArrayPoints)
+                print("ArrayPoints[IndexA]: ", ArrayPoints[IndexA])
+                print("ArrayPoints[IndexB]: ", ArrayPoints[IndexB])
+                # ArrayPoints.remove([ArrayPoints[IndexA]])
+                # ArrayPoints.remove([ArrayPoints[IndexB]])
+                Forrest.insert(TreeIdentifier, Tree)
                 TreeIdentifier = TreeIdentifier + 1 
-            #if the IndexA point has been added to a tree, but the other point has not, add other point to the IndexA point's tree
-            elif( (ArrayPoints[IndexA][2] == -1) and (ArrayPoints[IndexB][2] != -1) )
-                ArrayPoints[IndexA][2] = ArrayPoints[IndexB][2]
-            #if the IndexB point has been added to a tree, but the other point has not, add other point to the IndexB point's tree
-            elif( (ArrayPoints[IndexA][2] != -1) and (ArrayPoints[IndexB][2] == -1) )
+                NumTreesInForrest = NumTreesInForrest + 1
+
+            # if the IndexA point has been added to a tree, but the IndexB point has not, add the IndexB point
+            # to the IndexA point's tree and remove the IndexB point from the array of all other points
+            elif( (ArrayPoints[IndexA][2] != -1) and (ArrayPoints[IndexB][2] == -1) ):
                 ArrayPoints[IndexB][2] = ArrayPoints[IndexA][2]
+                print("Forrest[ ArrayPoints[IndexA][2] ] ", Forrest[ ArrayPoints[IndexA][2] ])
+                Forrest[ ArrayPoints[IndexA][2] ].insert(len(Forrest[ ArrayPoints[IndexA][2] ]), ArrayPoints[IndexB])
+                # ArrayPoints.remove([ArrayPoints[IndexB]])
+
+            # if the IndexB point has been added to a tree, but the IndexA point has not, add the IndexA point 
+            # to the IndexB point's tree and remove the IndexA point from the array of all other points
+            elif( (ArrayPoints[IndexA][2] == -1) and (ArrayPoints[IndexB][2] != -1) ):
+                ArrayPoints[IndexA][2] = ArrayPoints[IndexB][2]
+                print("Forrest[ ArrayPoints[IndexB][2] ]: ", Forrest[ ArrayPoints[IndexB][2] ])
+                Forrest[ ArrayPoints[IndexB][2] ].insert(len(Forrest[ ArrayPoints[IndexB][2] ]), ArrayPoints[IndexA])
+                # ArrayPoints.remove([ArrayPoints[IndexA]])
+
+            #if the both of the edge's points have been added to different trees, merge the trees.
             elif( ((ArrayPoints[IndexA][2] != -1) and (ArrayPoints[IndexB][2] != -1) ) and (ArrayPoints[IndexA][2] != ArrayPoints[IndexB][2]) ):
+                #keeping IndexA's TreeIdentifer, adding IndexB to IndexA's tree, removing IndexB from its own tree
+                ArrayPoints[IndexB][2] = ArrayPoints[IndexA][2]
+                Forrest[ ArrayPoints[IndexA][2] ].insert(len(Forrest[ ArrayPoints[IndexA][2] ]), ArrayPoints[IndexB])
+                Forrest[ ArrayPoints[IndexB][2] ].remove(ArrayPoints[IndexB])
 
+                #adding all element's from IndexB's former tree to IndexA's tree
+                while ( len(Forrest[ ArrayPoints[IndexB][2] ]) > 0 ):
+                    Forrest[ ArrayPoints[IndexA][2] ].insert(len(Forrest[ ArrayPoints[IndexA][2] ]), [ ArrayPoints[IndexB][2] ][0])
+                    Forrest[ ArrayPoints[IndexB][2] ].remove([ ArrayPoints[IndexB][2] ][0])
 
-        #adding every vertex not reached by E Prime as its own connected component
+                #replacing IndexB's former tree with "-1" in the forrest, to signify that a tree no longer exsists at that index
+                Forrest[ ArrayPoints[IndexB][2] ] = -1
+                NumTreesInForrest = NumTreesInForrest - 1
 
-    print("ArrayEPrime[0][0]: ", ArrayEPrime[0][0])
-    print("ArrayEPrime[0][1]: ", ArrayEPrime[0][1])
-    print("ArrayPoints[IndexA][2] :", ArrayPoints[ ArrayEPrime[0][0] ][2])
+        # every vertex not reached by E Prime still has a tree identifier value of -1, add each of these not yet reached vertices
+        # to the forrest as their own connected components. 
+        for i in range(0, len(ArrayPoints)):
+            if (ArrayPoints[i][2] == -1):
+                Tree = [ArrayPoints[i]]
+                Forrest.insert(i, Tree)
+                NumTreesInForrest = NumTreesInForrest + 1
+            
+    # print("ArrayEPrime[0][0]: ", ArrayEPrime[0][0])
+    # print("ArrayEPrime[0][1]: ", ArrayEPrime[0][1])
+    # print("ArrayPoints[IndexA][2] :", ArrayPoints[ ArrayEPrime[0][0] ][2])
     print("VNumVertices", VNumVertices)
     print("EPrimeNumElements", EPrimeNumElements)
     print("ArrayPoints", ArrayPoints)
@@ -128,8 +175,10 @@ def minimum_cost_connecting_edges(input_file_path, output_file_path):
     print("Sorted Edge Weights", SortedEdgeWeights)
     for e in range(ENumElements):
         print("Sorted --- Edge CoordinateA: [", SortedEdges[e].CoordinateA, "]  |  Edge CoordinateB: [", SortedEdges[e].CoordinateB, "]  | Edge Weight: ", SortedEdges[e].Weight)
-
-
+    print("NumTreesInForrest: ", NumTreesInForrest)
+    for e in range(NumTreesInForrest):
+            print("Tree in Forrest: ", Forrest[e])
+        
 
     result = -1
 
